041b124d18a7c324203eab46750b7371e67118d3
Polo94 <paul.henry.claudant@gmail.com>
2018-01-31 18:24:26 +0100

    add formulaire

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,8 +1,20 @@
 class GossipsController < ApplicationController
+ 
   def new
   	@gossip = Gossip.new
   end
-  def create
-  	
+
+  def create	
+  	@gossip = Gossip.new(gossip_params)
+  	@gossip.save
+  	redirect_to @gossip
   end
+
+  def show
+  @gossip = Gossip.find(params[:id])
+	end
+
+	def gossip_params
+	params.require(:gossip).permit(:anonymous_author, :content)
+	end
 end
